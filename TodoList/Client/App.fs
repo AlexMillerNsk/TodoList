2df494702049d9d744ab14716e7bff9cfd1ac900
user <user@WIN-PS5805E0O3P>
2022-05-14 12:09:51 +0700

    Добавьте файлы проекта.

---
+++ TodoList/Client/App.fs
@@ -0,0 +1,27 @@
+﻿module Todo.App
+
+open Elmish
+open Elmish.React
+open Elmish.Debug
+open Elmish.HMR
+
+// ====================================================
+// Construct the Elmish application using the three parts 
+// - State.initialState
+// - State.update
+// - View.render
+// ====================================================
+
+Program.mkProgram 
+  State.initialState 
+  State.update 
+  View.render
+#if DEBUG
+|> Program.withConsoleTrace
+#endif
+|> Program.withReact "elmish-app"
+#if DEBUG
+|> Program.withDebugger
+#endif
+|> Program.run
+
